9fa0b68224fbf0831153ff27d18f24049683bfc5
goeun23 <abgoeun3339@gmail.com>
2025-05-04 18:44:21 +0900

    pref: replace DataList DOM mapo with react-window for virtualization

--- components/DataList.tsx
+++ components/DataList.tsx
@@ -1,11 +1,22 @@
+import {FixedSizeList as List} from 'react-window'
+const Row = ({index, style, data})=> {
+  const item = data[index]
+  return (
+    <div style={style} className='py-1 border-b'>
+      {item.label}: {item.value}
+    </div>
+  )
+}
 export default function DataList({ data }) {
   return (
-    <ul className="overflow-auto h-[400px] border rounded p-2">
-      {data.map((item) => (
-        <li key={item.id} className="py-1 border-b">
-          {item.label}: {item.value}
-        </li>
-      ))}
-    </ul>
+    <List 
+      height={400}
+      itemCount={data.length}
+      itemSize={35}
+      width="100%"
+      itemData={data}
+    >
+      {Row}
+    </List>
   )
 }
