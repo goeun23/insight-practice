a592f09d04a2707c583792dce25f4e34052f2fc1
goeun23 <abgoeun3339@gmail.com>
2025-05-04 20:09:18 +0900

    feat: setup reportWebVitals in root for performance tracking

--- reportWebVitals.ts
+++ reportWebVitals.ts
@@ -1,5 +1,17 @@
-import type { Metric } from 'web-vitals'
+// reportWebVitals.ts
+import { getCLS, getFID, getLCP, getFCP, getTTFB, ReportHandler } from 'web-vitals'
 
-export function reportWebVitals(metric: Metric) {
-  console.log('[web-vitals]', metric.name, metric.value)
+export function reportWebVitals(onPerfEntry?: ReportHandler) {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    const wrappedHandler: ReportHandler = metric => {
+      console.log('[web-vitals]', metric.name, metric.value)
+      onPerfEntry(metric)
+    }
+
+    getCLS(wrappedHandler)
+    getFID(wrappedHandler)
+    getLCP(wrappedHandler)
+    getFCP(wrappedHandler)
+    getTTFB(wrappedHandler)
+  }
 }
